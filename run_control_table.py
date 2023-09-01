fc00537ee55543dd70d502da3b8f37db89882182
BortoletoYan <138122033+BortoletoYan@users.noreply.github.com>
2023-09-01 17:31:57 -0300

    feat: add-control

--- run_control_table.py
+++ run_control_table.py
@@ -2,153 +2,56 @@ import sys
 project_root = "C:\\Users\\User\\sites\\control-tower-D"
 sys.path.insert(0, project_root)
 from src.infra.database_connector import DatabaseConnection
-from src.errors.error_log import ErrorLog
 from datetime import datetime
-from src.stages.extract.extract import Extract
-from src.stages.extract.extract import ExtractHc
-from src.stages.transform.transform_sorting_in import TransformSorting
-from src.stages.transform.transform_putaway import TransformPutaway
-from src.stages.transform.transform_picking import TransformPicking
-from src.stages.transform.transform_sorting_out import TransformSortingOut
-from src.stages.transform.transform_packing import TransformPacking
-from src.stages.transform.transform_hc import TransformHc
-from src.stages.load.load_data import LoadData
-from src.drivers.sorting_in import SortingIn
-from src.drivers.putaway import Putaway
-from src.drivers.picking import Picking
-from src.drivers.sorting_out import SortingOut
-from src.drivers.packing import Packing
-from src.drivers.google_sheet_getter import GoogleSheetGetter
-from src.drivers.wms_report_upload import WmsReportUpload
-from src.infra.database_connector import DatabaseConnection
-from src.infra.database_repository import DatabaseRepository
-from src.queries.queries import INSERT_SORTING_IN as sorting_in_query
-from src.queries.queries import INSERT_PUTAWAY as putaway_query
-from src.queries.queries import INSERT_PICKING as picking_query
-from src.queries.queries import INSERT_SORTING_OUT as sorting_out_query
-from src.queries.queries import INSERT_PACKING as packing_query
-from src.queries.queries import INSERT_HC as hc_query
-from src.queries.queries import TRUNCATE_TABLE as truncate_query
-from src.queries.queries import RUN_PROCEDURE as procedure_query
-from src.errors.error_log import ErrorLog
+from src.main.main_pipeline import MainPipeline
 
 
 class PendingAtuomations:
-    def sorting_in(self):
-        try:
-            extract_sorting = Extract(SortingIn(), WmsReportUpload())
-            transform_sorting = TransformSorting()
-            load_sorting = LoadData(DatabaseRepository(query=sorting_in_query))
-            extract_sorting_in_contract = extract_sorting.extract()
-            transform_sorting_in_contract = transform_sorting.transform(
-                extract_sorting_in_contract
-            )
-            load_sorting.load(transform_sorting_in_contract)
-        except Exception as exception:
-            ErrorLog(str(exception), func="Sorting Pending Error")
-
-    def putaway(self):
-        try:
-            extract_putaway = Extract(Putaway(), WmsReportUpload())
-            transform_putaway = TransformPutaway()
-            load_putaway = LoadData(DatabaseRepository(query=putaway_query))
-            extract_putaway_in_contract = extract_putaway.extract()
-            transform_putaway_in_contract = transform_putaway.transform(
-                extract_putaway_in_contract
-            )
-            load_putaway.load(transform_putaway_in_contract)
-        except Exception as exception:
-            ErrorLog(str(exception), func="Putaway Pending Error")
-
-    def picking(self):
-        try:
-            extract_picking = Extract(Picking(), WmsReportUpload())
-            transform_picking = TransformPicking()
-            load_picking = LoadData(DatabaseRepository(query=picking_query))
-            extract_picking_in_contract = extract_picking.extract()
-            transform_picking_in_contract = transform_picking.transform(
-                extract_picking_in_contract
-            )
-            load_picking.load(transform_picking_in_contract)
-        except Exception as exception:
-            ErrorLog(str(exception), func="Picking Pending Error")
-
-    def sorting_out(self):
-        try:
-            extract_sorting_out = Extract(SortingOut(), WmsReportUpload())
-            transform_sorting_out = TransformSortingOut()
-            load_sorting_out = LoadData(DatabaseRepository(query=sorting_out_query))
-            extract_sorting_out_in_contract = extract_sorting_out.extract()
-            transform_sorting_out_in_contract = transform_sorting_out.transform(
-                extract_sorting_out_in_contract
-            )
-            load_sorting_out.load(transform_sorting_out_in_contract)
-        except Exception as exception:
-            ErrorLog(str(exception), func="Pipeline - Sorting_out")
-
-    def packing(self):
-        try:
-            extract_paking = Extract(Packing(), WmsReportUpload())
-            transform_packing = TransformPacking()
-            load_packing = LoadData(DatabaseRepository(query=packing_query))
-            extract_paking_in_contract = extract_paking.extract()
-            transform_packing_in_contract = transform_packing.transform(
-                extract_paking_in_contract
-            )
-            load_packing.load(transform_packing_in_contract)
-        except Exception as exception:
-            ErrorLog(str(exception), func="Pipeline - Packing")
-
-    def hc(self):
-        try:
-            extract_hc = ExtractHc(GoogleSheetGetter(), WmsReportUpload())
-            transform_hc = TransformHc()
-            load_hc = LoadData(DatabaseRepository(query=hc_query))
-            extract_hc_in_contract = extract_hc.extract()
-            transform_hc_in_contract = transform_hc.transform(extract_hc_in_contract)
-            load_hc.load(transform_hc_in_contract)
-        except Exception as exception:
-            ErrorLog(str(exception), func="Pipeline - HC")
-
-    def procedure(self):
-        try:
-            run_procedures = LoadData(DatabaseRepository(query=procedure_query))
-            run_procedures.procedure()
-        except Exception as exception:
-            ErrorLog(str(exception), func="Pipeline - Procedures")
-
+    pending_hour = []
+        
     def run_pending_automations(self) -> None:
         sectors = {
-            "Sorting_in": self.sorting_in,
-            "Putaway": self.putaway,
-            "Picking": self.picking,
-            "Sorting_out": self.sorting_out,
-            "Packing": self.packing,
+            "Sorting_in": MainPipeline.sorting_in,
+            "Putaway": MainPipeline.putaway,
+            "Picking": MainPipeline.picking,
+            "Sorting_out": MainPipeline.sorting_out,
+            "Packing": MainPipeline.packing,
             # Adicione aqui as demais funções
         }
 
+        DatabaseConnection.connect()
         cursor = DatabaseConnection.connection.cursor()
         current_time = datetime.now()
-        query = "SELECT id, name FROM control_automations WHERE scheduled_time <= %s AND status = False"
+        query = "SELECT id, name,scheduled_time FROM ware_ws_shein.control_automations WHERE scheduled_time <= %s AND status = False"
         cursor.execute(query, (current_time,))
         pending_automations = cursor.fetchall()
-        for automation_id, automation_name in pending_automations:
+        print(pending_automations[0][2])
+
+        for automation_id, automation_name, scheduled_time in pending_automations:
             try:
                 if automation_name in sectors:
                     sector = sectors[automation_name]
-                    sector() 
+                    # self.pending_hour = []
+                    # self.pending_hour.append(automation_id)
+                    sector(self)
                 else:
-                    print(
-                        f"Nenhuma função correspondente encontrada para '{automation_name}'"
-                    )
+                    print(f"No corresponding function found for '{automation_name}'")
 
-                # Marcar a automação como executada
-                self.hc()
-                self.procedure()
+                # Mark automation as executed
+                MainPipeline.hc(self)
+                MainPipeline.procedures(self)
+
                 update_query = "UPDATE automations SET status = 1 WHERE id = %s"
-                cursor.execute(update_query, (automation_id,))
-                DatabaseConnection.connection.commit()
+                with DatabaseConnection.connection.cursor() as cursor:
+                    cursor.execute(update_query, (automation_id,))
+                    DatabaseConnection.connection.commit()
 
-                print(f"Automação '{automation_name}' executada com sucesso.")
+                print(f"Automation '{automation_name}' executed successfully.")
+
             except Exception as e:
-                print(f"Erro ao executar a automação '{automation_name}': {str(e)}")
+                print(f"Error: {str(e)}")
+
+
+if __name__ == "__main__":
+    pending = PendingAtuomations()
+    pending.run_pending_automations()
