aed508302296c56e60bb545c85d901d8518ddb89
BortoletoYan <138122033+BortoletoYan@users.noreply.github.com>
2023-10-04 15:47:18 -0300

    feat: add-query

--- src/queries/queries.py
+++ src/queries/queries.py
@@ -3,11 +3,10 @@ INSERT_RC_MANAGEMENT = """
         (rc_warehouse, sortation_center_code,destination_warehouse,destination_warehouse_code,destination_warehouse_area,transfer_box_number,
         shelving_container_number,shipping_mode,same_park_or_not,forecast_status,number_of_sub-packages,creation_time,packer,packing_station,
         closing_time,closer,printing_time,printer,shipping_time,shipper,pickup_time,pickup_person,signed_for,signed_by,starting_time_of_shelving,completion_time,shelved_by,transfer_status
-
-
 )
     VALUES
-        (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+        (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+
 """
 
 INSERT_SORTING_IN = """
